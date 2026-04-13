99656d6492d785e5d35e9f3e619f4e3637417d3e
mansisonkar1909 <mansisonkar1909@gmail.com>
2026-04-14 02:32:57 +0530

    feat: initialize server.js with necessary imports and set working directory

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { chdir } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Force working directory to project root
+chdir(__dirname);
+
+// Now import index.js
 import('./src/index.js').catch(console.error);
