fe378cc1cd4622984390406046f339608e37fd64
mansisonkar1909 <mansisonkar1909@gmail.com>
2026-04-14 03:11:58 +0530

    refactor: remove unnecessary comments in server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ import { chdir } from 'process';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Force working directory to project root
 chdir(__dirname);
 
-// Now import index.js
 import('./src/index.js').catch(console.error);
