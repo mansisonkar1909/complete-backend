7f5f70481f92c980ca254f4397c4237f10c8b81a
mansisonkar1909 <mansisonkar1909@gmail.com>
2026-04-14 03:18:52 +0530

    refactor: enhance server.js by adding logging and restructuring import statements

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, resolve } from 'path';
 import { chdir } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Force working directory to where server.js is
 chdir(__dirname);
 
-import('./src/index.js').catch(console.error);
+console.log('Current directory:', process.cwd());
+console.log('Dirname:', __dirname);
+
+// Import index.js relatively
+import(__dirname + '/src/index.js').catch(console.error);
