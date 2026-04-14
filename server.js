7c27ddfccbc00565beee5efccbca31e4088f863e
mansisonkar1909 <mansisonkar1909@gmail.com>
2026-04-15 00:39:34 +0530

    refactor: simplify server.js by removing commented code and enhancing dynamic import

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { chdir } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log('Server.js location:', __dirname);
-console.log('Starting app from:', __dirname + '/src/index.js');
+chdir(__dirname);
 
-// Dynamically import with full absolute path
-const indexPath = new URL('./src/index.js', import.meta.url).href;
-console.log('Index path:', indexPath);
-
-import(indexPath).catch((err) => {
-    console.error('Failed to start:', err);
-    process.exit(1);
-});
+const indexUrl = new URL('file://' + __dirname + '/src/index.js');
+import(indexUrl).catch(console.error);
